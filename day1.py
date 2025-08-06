2fcf7ddab2a3a71e531d3caf56cdc3a0471a5931
Dr-vegapunk <Ishankarki7624@gmail.com>
2025-08-06 08:25:52 +0545

    day1 python script

---
+++ day1.py
@@ -0,0 +1,17 @@
+print("Hello world")
+
+name= 'Ishan karki'
+age = 26
+
+print(f"My name is {name} and I am {age} years old")
+
+
+userName=input("Enter username: ")
+age=int(input("Enter age: "))
+password=input("Enter password: ")
+email=input("Enter email: ")
+
+print(f"User Information:")
+print(f"Username: {userName}")
+print(f"Age: {age}")
+print(f"Email: {email}")
